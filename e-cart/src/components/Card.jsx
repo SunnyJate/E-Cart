55c9859c89a8b85bca2a2ccc29ca50732731a1f5
Sunny Jate <sunnyjate2007@gmail.com>
2024-09-02 14:18:51 +0530

    State are updated in app.js and css style is applied

--- e-cart/src/components/Card.jsx
+++ e-cart/src/components/Card.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import "../styles/card.css"
 
-const Card = ({ item }) => {
+const Card = ({ item, handleClick }) => {
+  const{title,author,price,img} = item;
   return (
     <div className="cards">
       <div className="image_box">
-        <img src={item.img} alt="image"/>
+        <img src={img} alt="image"/>
+      </div>
+      <div className="details">
+        <p>{title}</p>
+        <p>{author}</p>
+        <p>Price - {price} ₹</p>
+        <button onClick={()=>handleClick(item)}>Add to Cart</button>
       </div>
     </div>
   );
